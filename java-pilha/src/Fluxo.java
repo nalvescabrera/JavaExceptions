8ac27856cb4f1f8af5282045a20a9c3a555ba36b
Natalia Cabrera <n.alvescabrera@gmail.com>
2021-05-31 20:30:50 -0300

    Adicionado classes com finally, checked e unchecked

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -2,12 +2,13 @@ import java.util.Iterator;
 
 public class Fluxo {
 
+	/* MinhaExcecao --> checked, no qual você pode colocar no metodo throw ou cria um try/catch */
 	
 	public static void main(String[] args) {
 		System.out.println("Ini do Main");
 		try {
 			metodo1();
-		} catch(ArithmeticException | NullPointerException | MinhaExcecao ex) {
+		} catch(Exception ex) {
 			String msg = ex.getMessage();
 			System.out.println("Exception " + msg);
 			ex.printStackTrace();
@@ -15,16 +16,16 @@ public class Fluxo {
 		System.out.println("Fim do Main");
 	}
 	
-	public static void metodo1() {
+	public static void metodo1() throws MinhaExcecao {
 		System.out.println("Inicio do metodo1");
 		metodo2();
 		System.out.println("Fim do metodo1");
 	}
 	
-	public static void metodo2() {
+	public static void metodo2() throws MinhaExcecao {
 		System.out.println("Inicio do metodo2");
 		
-		throw new MinhaExcecao("Deu muito errado");
+		new MinhaExcecao("Deu muito errado");
 		//System.out.println("Fim do metodo2");
 	}
 }
--- java-pilha/src/FluxoComError.java
+++ java-pilha/src/FluxoComError.java
@@ -7,7 +7,7 @@ public class FluxoComError {
 		System.out.println("Ini do Main");
 		try {
 			metodo1();
-		} catch(ArithmeticException | NullPointerException | MinhaExcecao ex) {
+		} catch(ArithmeticException | NullPointerException ex) {
 			String msg = ex.getMessage();
 			System.out.println("Exception " + msg);
 			ex.printStackTrace();
@@ -15,13 +15,13 @@ public class FluxoComError {
 		System.out.println("Fim do Main");
 	}
 	
-	public static void metodo1() {
+	public static void metodo1(){
 		System.out.println("Inicio do metodo1");
 		metodo2();
 		System.out.println("Fim do metodo1");
 	}
 	
-	public static void metodo2() {
+	public static void metodo2(){
 		System.out.println("Ini do metodo 2");
 		metodo2();
 		System.out.println("Fim do metodo 2");
